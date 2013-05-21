8daf48fc4f1439e864e07e39dfe914d512307659
ndizigiye <ndiarmand11@yahoo.fr>
2013-05-22 01:29:20 +0200

    new changes

--- src/com/alfresco/Test.java
+++ src/com/alfresco/Test.java
@@ -8,9 +8,9 @@ import com.sun.jersey.api.client.WebResource;
 public class Test {
 
 	// geef hier je gegevens
-	static String url = "";
-	static String username = "";
-	static String password = "";
+	static String url = "http://alfrescocommunity-42337-ndiarmand.cloudwaysapps.com";
+	static String username = "admin";
+	static String password = "bowmnhgdx4";
 	static String ticket = "";
 
 	public Test() {
@@ -18,7 +18,7 @@ public class Test {
 	}
 
 	public String getTicket() {
-		String resource_url = "";
+		String resource_url = url+"/alfresco/service/api/login?u="+username+"&pw="+password;
 		Client c = Client.create();
 		WebResource r = c.resource(resource_url);
 		r.accept(MediaType.APPLICATION_JSON_TYPE,MediaType.APPLICATION_XML_TYPE);
