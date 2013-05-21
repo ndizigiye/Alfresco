e2c4ee7da645c3f88e7769efb1bffc24af6fadc4
ndizigiye <ndiarmand11@yahoo.fr>
2013-05-22 01:29:20 +0200

    new changes

--- src/com/alfresco/Test.java
+++ src/com/alfresco/Test.java
@@ -1,6 +1,18 @@
 package com.alfresco;
 
+import java.io.IOException;
+import java.io.StringReader;
+
 import javax.ws.rs.core.MediaType;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
@@ -13,11 +25,18 @@ public class Test {
 	static String password = "bowmnhgdx4";
 	static String ticket = "";
 
-	public Test() {
+	public Test(){
+		
+		try {
+			ticket = this.getTicket();
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
-	public String getTicket() {
+	public String getTicket() throws ParserConfigurationException, SAXException, IOException {
 		String resource_url = url+"/alfresco/service/api/login?u="+username+"&pw="+password;
 		Client c = Client.create();
 		WebResource r = c.resource(resource_url);
@@ -25,12 +44,19 @@ public class Test {
 		r.header("X-FOO", "BAR");
 		String xml = r.get(String.class);
 		
-		return xml;
+		DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+	    InputSource is = new InputSource();
+	    is.setCharacterStream(new StringReader(xml));
+	    Document doc = db.parse(is);
+	    String ticket = doc.getElementsByTagName("ticket").item(0).getTextContent();
+	    
+		return ticket;
    
 	}
 
 	public String getUsers() {
 
+		return null;
 	}
 
 }
